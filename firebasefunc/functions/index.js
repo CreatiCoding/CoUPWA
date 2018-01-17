77180df35ff3b70eeac0bd2df8bea6b8c63b09ba
Linkaden <tentasys@gmail.com>
2018-01-17 15:27:24 +0900

    function test

---
+++ firebasefunc/functions/index.js
@@ -0,0 +1,20 @@
+const functions = require('firebase-functions');
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+exports.bigben = functions.https.onRequest((req, res) => {
+  const hours = (new Date().getHours() % 12) + 9 // london is UTC + 1hr;
+  res.status(200).send(`<!doctype html>
+    <head>
+      <title>Time</title>
+    </head>
+    <body>
+      ${'BONG '.repeat(hours)}
+    </body>
+  </html>`);
+});
