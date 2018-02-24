69c467d2d2209dd12b60756d38f9aed2e371228d
creaticoder <creaticoding@gmail.com>
2018-02-25 00:17:53 +0900

    test(travis): for init, test about travis-ci with nodejs

---
+++ travis_test/hello.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Travis!\n')		// error code for test
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
