bc690ec86aa962073c08fc08a82601accc8b1a24
kyungjin1121 <yyouyeon1121@gmail.com>
2018-02-25 11:22:24 +0900

    chore(project): unuesd directory needs to be deleted.

--- project/public/firebase/init.js
+++
@@ -1,9 +0,0 @@
-if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
-firebase.initializeApp({
-  "apiKey": "AIzaSyD88cDgm2aG4i66QyR7OsFHEiigWaW-etw",
-  "databaseURL": "https://coupwa.firebaseio.com",
-  "storageBucket": "coupwa.appspot.com",
-  "authDomain": "coupwa.firebaseapp.com",
-  "messagingSenderId": "323516523754",
-  "projectId": "coupwa"
-});
