c99a327db72510cf1fb7c91fb4933a274258fd8f
kyungjin1121 <yyouyeon1121@gmail.com>
2018-03-12 14:43:39 +0900

    feature(controller): input of function

--- project-v2/functions/controller.js
+++ project-v2/functions/controller.js
@@ -1,3 +1,4 @@
+// firebase experimental:functions:shell
 const admin = require('firebase-admin');
 
 //IndexedDB 안에 사용자 UUID가 E/NE
@@ -6,33 +7,40 @@ const admin = require('firebase-admin');
 
 
 //IndexedDB(TEST)
-exports.testFc = function(){
+/* TODO 사용자의 입력을 함수 인자로 받는 방법  */
+exports.testFc = function(uuid, toon){
 	const db = admin.firestore();
 	var docRef = db.collection('IndexedDB');
 
+	//임의로 등록
 	docRef.doc('user1').set({
 		uuid:'user1'});
 	docRef.doc('user2').set({
 		uuid:'user2'
 	});
 
-	//docRef 참조위치 지정
-	//docRef = db.collection('IndexedDB').doc('user1');
+	/* TODO USER inputs uuid value */
+	//Reference location of docRef is uuid value
+	docRef = db.collection('IndexedDB').doc(uuid);
 
 	//UUID가 존재하지 않을 때(test)
-	docRef = db.collection('IndexedDB').doc('user3');
+	//docRef = db.collection('IndexedDB').doc('user3');
 
 	//search uuid in IndexedDB
 	docRef.get().then(function (doc) {
 		if(doc.exists){
 			console.log("Document data: ", doc.data());
 			//관심웹툰추가
-			addToon(docRef);
+			addToon(docRef,toon);
 		}
 		else{
 			console.log("No such document!");
 			//사용자  UUID 추가
-			addUser(docRef);
+			addUser(docRef,uuid);
+
+			/* TODO 관심웹툰 추가 */
+			addToon(docRef, toon);
+
 		}
 	}).catch(function (error) {
 		console.log("Error getting document:", error);
@@ -41,15 +49,16 @@ exports.testFc = function(){
 }
 
 //IndexedDB에 사용자 UUID가 없을 때
-function addUser(docRef){
+function addUser(docRef, uuid){
 	docRef.set({
-		uuid: 'new user'
+		uuid: uuid
 	})
 }
 
 //IndexedDB에 사용자 UUID가 있을 때 -> 선호 웹툰 업데이트
-function addToon(docRef){
+function addToon(docRef, toon){
+	/* TODO 사용자의 관심 웹툰 업데이트 */
 	docRef.update({
-		Like: '대학일기'
+		Like: toon
 	})
 }
