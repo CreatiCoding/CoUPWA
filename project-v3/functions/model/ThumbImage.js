8ff1fe1bdbcfc2fab0bb79f195883bbc66c619fd
JeongSeokHo <creaticoding@gmail.com>
2018-05-03 10:04:23 +0900

    feat(model): modify idx column to convention

--- project-v3/functions/model/ThumbImage.js
+++ project-v3/functions/model/ThumbImage.js
@@ -1,6 +1,6 @@
 //파일 정보
 module.exports = function ThumbImage(thumb_idx, thumb_url, toon_info_idx) {
-	this.thumb_idx = thumb_idx;
+	this.thumb_image_idx = thumb_idx;
 	this.thumb_url = thumb_url;
 	this.toon_info_idx = toon_info_idx;
 };
