206d5899b30c2191aae7d7cdc5ca07969a1279d2
JeongSeokHo <creaticoding@gmail.com>
2018-05-04 11:33:17 +0900

    repectory(index): just select first ele without parsing text

--- project-v3/functions/controller/BannerImage.js
+++ project-v3/functions/controller/BannerImage.js
@@ -17,26 +17,15 @@ const self = {
 			.then(result2 => {
 				let arr = [];
 				result2.map(ele => {
-					for (let key in ele) {
-						// 인덱스 이름의 컬럼의 값은 /라는 문자를 _로 바꾸어 저장한다.
-						// 파이어스토어의 path 혼동 방지용
-						// 키값을 이용하여 인덱스이름을 구해내고 그 값을 바꿈.
-						let idxName = "";
-						key.split("").map((e, i) => {
-							if (e !== e.toLowerCase())
-								idxName += "_" + e.toLowerCase();
-							else idxName += e;
-						});
-						idxName += "_idx";
-						ele[key][idxName] = ele[key][idxName].replace(
+					for (let attr in ele) {
+						let key = ele[attr][Object.keys(ele[attr])[0]].replace(
 							/\//gi,
 							"_"
 						);
-						// fs.collection(model).doc(key) <- data 넣을 데이터
 						arr.push({
-							model: key,
-							key: ele[key][idxName],
-							data: ele[key]
+							model: attr,
+							key: key,
+							data: ele[attr]
 						});
 					}
 				});
