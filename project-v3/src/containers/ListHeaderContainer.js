d461261f308faa253e90a6b6476ab7ea9df194d4
JeongSeokHo <creaticoding@gmail.com>
2018-05-15 12:24:31 +0900

    feat(list): design about header part

--- project-v3/src/containers/ListHeaderContainer.js
+++ project-v3/src/containers/ListHeaderContainer.js
@@ -1,8 +1,5 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
-import * as actions from "../actions";
-import $ from "jquery";
-import coupwaFetch from "../lib/coupwaFetch";
 import "../css/ListHeader.css";
 import {Link} from "react-router-dom";
 
@@ -10,27 +7,41 @@ class ListHeaderContainer extends Component {
 	constructor(props) {
 		super(props);
 	}
-	componentDidMount() {}
+	componentDidMount() {
+		console.log(this.props.toonInfo);
+	}
 	render() {
 		return (
-			<div className="header">
-				<span className="backbutton">
-					<Link to="/">◀</Link>
-				</span>
-				<span>웹툰명</span>
-				<span className="like">
-					<a href="#">관심</a>
-				</span>
-				<span className="first-chapter">
-					<a href="#">첫화보기</a>
-				</span>
+			<div className="list-header-container">
+				<div className="list-header">
+					<div>
+						<div className="list-header-left">
+							<span className="backbutton">
+								<Link to="/">&lt;</Link>
+							</span>
+							<span className="list-header-title">
+								{this.props.toonInfo.toon_info_title_name}
+							</span>
+						</div>
+						<div className="list-header-right">
+							<span className="like">
+								<a href="#">관심</a>
+							</span>
+							<span className="first-chapter">
+								<a href="#">첫화보기</a>
+							</span>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
 }
 
 const mapStateToPrpos = state => {
-	return {};
+	return {
+		toonInfo: state.listContentsReducer.toonInfo
+	};
 };
 
 const mapDispatchToProps = dispatch => {
