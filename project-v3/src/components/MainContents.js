a600a0ad99009ebb15a94494a3adbd03ab503dd4
JeongSeokHo <creaticoding@gmail.com>
2018-05-15 10:51:05 +0900

    feat(main-design):little design modify

--- project-v3/src/components/MainContents.js
+++ project-v3/src/components/MainContents.js
@@ -3,12 +3,9 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../css/MainContents.css";
+import {Link} from "react-router-dom";
 
-const MainContents = ({
-	viewToon
-	// changeWeekNum
-	//sortType /*, currentWeekNum, changeWeekNum*/
-}) => {
+const MainContents = ({viewToon}) => {
 	let initSlide = new Date().getDay() == 0 ? 6 : new Date().getDay() - 1;
 	let settings = {
 		// 0이면 일요일인데 일요일 슬라이드는 6임 나머진-1만 해주면됨
@@ -25,66 +22,70 @@ const MainContents = ({
 		customPaging: function(i) {
 			let dow = ["월", "화", "수", "목", "금", "토", "일"];
 			return <a>{dow[i]}</a>;
-		},
-		afterChange: (currendSlide, dummy) => {
-			console.log("afterChange, currendSlide", currendSlide);
-			//setTimeout(function() {
-			//console.log("prevSlide", prevSlide);
-			//changeWeekNum(prevSlide);
-			//}, 100);
-		},
-		beforeChange: (dummy, nextSlide) => {
-			// changeWeekNum(nextSlide);
-			console.log("beforeChange, nextSlide", nextSlide);
-			// setTimeout(function() {
-			// console.log("nextSlide", nextSlide);
-			// 	changeWeekNum(nextSlide);
-			// }, 1);
 		}
 	};
 
 	const mapToMainContentsWebtoon = data => {
 		return data.map((ele, seq) => {
+			let isMouseDown = false;
+			let isLinkable = true;
 			return (
-				<div
-					className="main-contents-toon col-xl-2 col-lg-2 col-md-2 col-sm-3 col-4"
+				<Link
 					key={seq}
+					to={"/list/" + ele.toon_info_idx}
+					onMouseMove={() => {
+						if (isMouseDown) {
+							isLinkable = false;
+						}
+					}}
+					onMouseDown={() => {
+						isMouseDown = true;
+					}}
+					onMouseUp={() => {
+						isMouseDown = false;
+					}}
+					onClick={e => {
+						if (!isLinkable) {
+							isLinkable = true;
+							e.preventDefault();
+						}
+					}}
 				>
-					<div className="main-contents-toon-img">
-						<img
-							className="main-contents-toon-thumbnail"
-							src={ele.image_path}
-						/>
-						<img
-							className="default-thumbnail"
-							src={"/images/thumbnail_default.png"}
-						/>
-					</div>
-					<div className="main-contents-toon-info">
-						<div className="main-contents-toon-title">
-							{ele.toon_info_name}
+					<div className="main-contents-toon col-xl-2 col-lg-2 col-md-2 col-sm-3 col-4">
+						<div className="main-contents-toon-img">
+							<img
+								className="main-contents-toon-thumbnail"
+								src={ele.image_path}
+							/>
+							<img
+								className="default-thumbnail"
+								src={"/images/thumbnail_default.png"}
+							/>
 						</div>
-						<div className="main-contents-toon-middle">
-							<div className="main-contents-toon-star">
-								{ele.toon_info_star}
+						<div className="main-contents-toon-info">
+							<div className="main-contents-toon-title">
+								{ele.toon_info_name}
 							</div>
-							<div>&nbsp;</div>
-							<div className="main-contents-toon-update">
-								{ele.toon_info_update.toString()}
+							<div className="main-contents-toon-middle">
+								<div className="main-contents-toon-star">
+									{ele.toon_info_star}
+								</div>
+								<div>&nbsp;</div>
+								<div className="main-contents-toon-update">
+									{ele.toon_info_update.toString()}
+								</div>
+							</div>
+							<div className="main-contents-toon-author">
+								{ele.toon_info_author}
 							</div>
 						</div>
-						<div className="main-contents-toon-author">
-							{ele.toon_info_author}
-						</div>
 					</div>
-				</div>
+				</Link>
 			);
 		});
 	};
 
 	const mapToSlideToMainContents = viewToon => {
-		console.log(viewToon);
-		// let week = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 		return viewToon.map((ele, seq) => {
 			return (
 				<div className="container" key={seq}>
